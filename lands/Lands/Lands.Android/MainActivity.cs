ba66e062f0664683a27c5a124cdfe0eb644373dd
jgomez881 <jgomez88@eafit.edu.co>
2018-03-15 14:18:43 -0500

    V3

--- lands/Lands/Lands.Android/MainActivity.cs
+++ lands/Lands/Lands.Android/MainActivity.cs
@@ -12,8 +12,7 @@ namespace Lands.Droid
     [Activity(Label = "Lands", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
-        {
+        protected override void OnCreate(Bundle bundle){
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
